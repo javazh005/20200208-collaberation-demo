7365aa039e6fab6127540a4ef452a952c8f0eba4
paul <paul860513@gmail.com>
2020-02-08 16:48:32 +0800

    local update 04:48

--- paul.js
+++ paul.js
@@ -1,1 +1,18 @@
 console.log("hello~~~");
+
+function calculate(){
+    quantity = document.getElementById('quantity').value;
+    price = document.getElementById('price').value;
+    tax = document.getElementById('tax').value;
+    discount = document.getElementById('discount').value;
+    total = quantity * price;
+    tax = tax/100;
+    tax++;
+    total *= tax;
+    total-=discount;
+    total = total.toFixed(2);
+    
+    document.getElementById('total').innerText = total;
+
+    return false; //不回傳 讓total留在畫面
+}
