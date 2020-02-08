a2416b62259a0c75659c6a7c62764df7f6526684
james <james96317@gmail.com>
2020-02-08 16:55:05 +0800

    greg

--- james.js
+++ james.js
@@ -1,11 +1,146 @@
 window.for (const item in object) {
 
-    const
-    gregreger
-    gregregergerrgerg
-    gregregrgrereger
+    <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>fieldset</title>
+    <link rel="stylesheet" href="2_fieldset_main.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                User Registration Form
+                <fieldset>
+                    <legend class="a02"><span>P</span>ersonal Information </legend>
 
+                    
+            <form action="">
+                <p >
+                    <label for="">Name:</label>
+                    <input type="text">
+                </p>
+                <p >
+                    <label for="">Email Address:</label>
+                    <input type="email">
+                </p>
+                <p >
+                    <label for="">Password:</label>
+                    <input type="password">
+                </p>
+                <p >
+                    <label for="">Confirm Password:</label>
+                    <input type="text">
+                </p>
 
+            </form>
+            <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>fieldset</title>
+    <link rel="stylesheet" href="2_fieldset_main.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                User Registration Form
+                <fieldset>
+                    <legend class="a02"><span>P</span>ersonal Information </legend>
+
+                    
+            <form action="">
+                <p >
+                    <label for="">Name:</label>
+                    <input type="text">
+                </p>
+                <p >
+                    <label for="">Email Address:</label>
+                    <input type="email">
+                </p>
+                <p >
+                    <label for="">Password:</label>
+                    <input type="password">
+                </p>
+                <p >
+                    <label for="">Confirm Password:</label>
+                    <input type="text">
+                </p>
+
+            </form>
+            <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>fieldset</title>
+    <link rel="stylesheet" href="2_fieldset_main.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                User Registration Form
+                <fieldset>
+                    <legend class="a02"><span>P</span>ersonal Information </legend>
+
+                    
+            <form action="">
+                <p >
+                    <label for="">Name:</label>
+                    <input type="text">
+                </p>
+                <p >
+                    <label for="">Email Address:</label>
+                    <input type="email">
+                </p>
+                <p >
+                    <label for="">Password:</label>
+                    <input type="password">
+                </p>
+                <p >
+                    <label for="">Confirm Password:</label>
+                    <input type="text">
+                </p>
+
+            </form>
+            <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>fieldset</title>
+    <link rel="stylesheet" href="2_fieldset_main.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                User Registration Form
+                <fieldset>
+                    <legend class="a02"><span>P</span>ersonal Information </legend>
+
+                    
+            <form action="">
+                <p >
+                    <label for="">Name:</label>
+                    <input type="text">
+                </p>
+                <p >
+                    <label for="">Email Address:</label>
+                    <input type="email">
+                </p>
+                <p >
+                    <label for="">Password:</label>
+                    <input type="password">
+                </p>
+                <p >
+                    <label for="">Confirm Password:</label>
+                    <input type="text">
+                </p>
+
+            </form>
+            
 
     <!DOCTYPE html>
 
